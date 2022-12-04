ccb8cce5cff6c95f54073af3338a7f7315d827b6
Maddy <maddyabbas76@gmail.com>
2022-12-05 03:13:57 +0500

    Applying new more steps.

--- Javascript Starter/script.js
+++ Javascript Starter/script.js
@@ -122,3 +122,84 @@ console.log(now - 1991 > now - 2017);
 
 const averageAge = (ageJonas + ageSarah) / 2;
 console.log(ageJonas, ageSarah, averageAge);
+
+/*
+// Coding Challenge #1
+
+Mark and John are trying to compare their BMI (Boday Mass Index), which is calculated using the formula: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter)
+
+1. Store Mark's and John's mass and height in variables
+2. Calculated both their BMIs using the formula (you can even implement both versions)
+3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
+
+TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weight 92 kg and is 1.95 m tall.
+TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weight 85 kg and is 1.76 m tall.
+
+Good Luck :)
+*/
+
+// const heightMark = 1.69;
+// const weightMark = 78;
+// const heightJohn = 1.95;
+// const weightJohn = 92;
+const heightMark = 1.88;
+const weightMark = 95;
+const heightJohn = 1.76;
+const weightJohn = 85;
+let markBMI = weightMark/heightMark ** 2;
+console.log(markBMI);
+let johnBMI = weightJohn/heightJohn ** 2;
+console.log(johnBMI);
+console.log(markBMI > johnBMI);
+
+//String and Template Litertal 
+const first_Name = 'Maddy';
+const Hobby = 'Student';
+const birth_Year = 1991;
+const _year = 2037;
+
+const Maddy = "I'm " + first_Name + ', a ' + (_year - birth_Year) + ' years old ' + Hobby + '!';
+console.log(Maddy);
+const maddyNew = `I'm ${first_Name}, a ${_year - birth_Year} years old ${Hobby}!`;
+console.log(maddyNew);
+
+console.log(`Just a regular string.....`);
+console.log('String with \n\
+multiple \n\
+lines');
+console.log(`String with
+multiple
+lines`);
+
+// Taking Decisions if_else Statements
+
+const currentAge = 18;
+if(currentAge >= 18) {
+    console.log('Sarah can start driving license 🚗');
+} else {
+    const yearLeft = 18 - currentAge;
+    console.log(`Sarah is too young. Wait another ${yearLeft} years :)`);
+}
+
+const birthday = 1997;
+let century;
+if(birthday <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);
+/*
+Coding Challenge #2
+Use the BMI example from challenge #1, and the code you already wrote, and improve it:
+1. Print a nice output to the console, saying who has the higher BMI. The message can be either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's BMI (28.3) is higher than John's (23.9)!"
+HINT: Use an if/else statement 😁.
+GOOD LUCK :)
+*/
+ 
+if(markBMI > johnBMI) {
+    console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI}0!`);
+} else {
+    console.log(`John's BMI (${johnBMI}) is higher than Mark's BMI (${markBMI})!`);
+}
