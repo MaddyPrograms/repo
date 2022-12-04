e087e6350f51075439fe3f8c70f0f3204dbf5cca
Maddy <maddyabbas76@gmail.com>
2022-12-05 03:58:13 +0500

    Learnt Type Conversion in this section.

--- Javascript Starter/script.js
+++ Javascript Starter/script.js
@@ -204,3 +204,22 @@ if(markBMI > johnBMI) {
     console.log(`John's BMI (${johnBMI}) is higher than Mark's BMI (${markBMI})!`);
 }
 
+// Type conversion 
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+//Type Coerction 
+console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' -3);
+console.log('23' / '2');
+
+let n = '1' + 1; // '11'
+n = n - 1;
+console.log(n);
+
