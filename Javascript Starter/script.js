c0f1eec372fda867b602eeada67bee814ca28981
Maddy <maddyabbas76@gmail.com>
2022-12-04 20:47:03 +0500

    Update with javascript

---
+++ Javascript Starter/script.js
@@ -0,0 +1,124 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);  
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Bob";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let jonas_matilda = "JM";
+let $function = 27;
+let person = "Jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher"
+
+console.log(myFirstJob);
+
+// let country = "Pakistan";
+// let population = "22 Crore";
+// console.log(country);
+// console.log(population);
+
+true;
+let javaScriptIsFunn = true;
+console.log(javaScriptIsFunn);
+
+console.log(typeof true);
+console.log(typeof javaScriptIsFunn);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+// Dynamic Typing practice Here
+javaScriptIsFunn = 'YES!';
+console.log(typeof javaScriptIsFunn);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+//Assignment
+let isIsland = true;
+console.log(typeof isIsland);
+isIsland = "Pakistan"
+console.log(typeof "Pakistan");
+console.log(typeof "population");
+console.log(typeof "country");
+console.log(typeof "language");
+
+// Variable & Constant
+let age = 30;
+age = 31;
+
+const birhtYear = 2000;
+var job = 'programmer';
+job = 'teacher';
+
+let language = "English";
+
+//Maths Operator
+const now =  2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2017;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+// const firstName =  "Maddy";
+// const lastName = "Abbas";
+// console.log(firstName+' '+lastName);
+
+//Assignment operators
+let x = 10+5; // 15
+x += 10; //x = x + 10 = 25
+x *= 4; //x = x * 4 = 100
+x++; // x=x+1
+x--; // x=x-1;
+x--; 
+console.log(x);
+
+// Comparison Operator 
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+console.log(now - 1991 > now - 2017);
+
+//Basic Operator Assignment
+let country = "Pakistan";
+let population =  "22";
+firstPart = population/2;
+console.log(firstPart);
+secondPart = 22 - firstPart;
+console.log(secondPart);
+
+population++;
+console.log(population);
+
+let findLand_population = 66;
+console.log(population === findLand_population);
+console.log(population >= findLand_population);
+console.log(population <= findLand_population);
+
+const description = "'Portugal is in Europe, and its 11 million people speak portuguese";
+console.log(description);
+
+
+console.log(now - 1991 > now - 2017);
+
+// let x, y;
+// x = y = 25 - 10 - 5; // x = y = 10, x =10
+// console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
